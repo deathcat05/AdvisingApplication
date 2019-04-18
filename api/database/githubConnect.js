67be1b20eb8f2b03740afea3c5934a95e6f75de6
alabossiere <labossiere@sonoma.edu>
2019-04-18 11:47:51 -0700

    Todays Progress

--- api/database/githubConnect.js
+++ api/database/githubConnect.js
@@ -2,11 +2,11 @@ let mysql = require('mysql');
 
 let connection = mysql.createConnection({
   debug: false,
-  host: '',
-	port: ,
-	user: '',
-	password:'',
-	database: ''
+  host: 'blue.cs.sonoma.edu',
+	port: 8140,
+	user: 'cs386_alabossi',
+	password:'la5117',
+	database: 'cs386_alabossi'
 });
 
 module.exports = connection;
