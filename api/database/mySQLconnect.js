236a37b518eda667f9f9ce21554d06736843048f
nieroda <eathotlead@gmail.com>
2019-04-23 09:31:09 -0700

    stuffs

---
+++ api/database/mySQLconnect.js
@@ -0,0 +1,12 @@
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+  debug: false,
+  host: 'blue.cs.sonoma.edu',
+	port: 3306,
+	user: 'cs386_nkamm',
+	password:'ka3287',
+	database: 'cs386_nkamm'
+});
+
+module.exports = connection;
